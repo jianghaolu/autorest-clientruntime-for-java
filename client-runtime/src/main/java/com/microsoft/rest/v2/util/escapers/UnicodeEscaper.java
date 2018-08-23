185bef7d469c59bfbd58a4e67f2487d1ef5101c4
Jianghao Lu <jianghaolu@users.noreply.github.com>
2018-08-23 14:20:22 -0700

    Modify license according to CELA

--- client-runtime/src/main/java/com/microsoft/rest/v2/util/escapers/UnicodeEscaper.java
+++ client-runtime/src/main/java/com/microsoft/rest/v2/util/escapers/UnicodeEscaper.java
@@ -1,5 +1,6 @@
 /*
- * ORIGINAL Copyright (C) 2009 The Guava Authors
+ * Copyright (C) 2009 The Guava Authors
+ * Portions Copyright (c) Microsoft Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -11,9 +12,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  *
- * MODIFIED Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
  */
 
 package com.microsoft.rest.v2.util.escapers;
